b464fa6c042357610afe69e3244d2b2b87443709
Holrem <c.t.grinnell@gmail.com>
2024-07-15 14:03:23 -0500

    updated main.py to count each character

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     print(f"{num_words} words found in the document")
+    char_count = get_word_amount(text)
+    print(char_count)
 
 
 def get_num_words(text):
@@ -13,6 +15,17 @@ def get_num_words(text):
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+def get_word_amount(text):
+    lowercase = text.lower()
+    chars = {}
+    for char in lowercase:
+        if char in chars:
+            chars[char] += 1
+        else:
+            chars[char] = 1
+    return chars
+    
 
 
 main()
